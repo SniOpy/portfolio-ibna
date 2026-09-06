04f6bcaadb4553bc8e8eedb8a6a144e71c1d5442
SniOpy <soufianeibna@gmail.com>
2026-09-06 15:50:59 +0100

    🚧 Show first portfolio content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Soufiane Ibnaiche</h1>
+      <h2>Développeur React, Typescript et Node.js</h2>
+      <h3>Je conçois des interfaces performantes et des applications web orientées métier.</h3>
+    </>
+  );
 }
 
 export default App;
