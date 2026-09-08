d6aacbf126cc3878794f1554e7c46fefc7abd274
SniOpy <soufianeibna@gmail.com>
2026-09-08 11:43:29 +0100

    ✨  feat : validate form data

--- src/components/form/RecruiterForm.tsx
+++ src/components/form/RecruiterForm.tsx
@@ -9,6 +9,15 @@ type RecruiterFormData = {
   message: string;
 };
 
+type RecruiterFormErrors = {
+  name?: string;
+  company?: string;
+  email?: string;
+  contract?: string;
+  workMode?: string;
+  message?: string;
+};
+
 const initialFormData: RecruiterFormData = {
   name: '',
   company: '',
@@ -19,8 +28,11 @@ const initialFormData: RecruiterFormData = {
 };
 
 function RecruiterForm() {
+  // State
   const [formData, setFormData] = useState<RecruiterFormData>(initialFormData);
+  const [formErrors, setFormErrors] = useState<RecruiterFormErrors>({});
 
+  // Comportement
   const handleChange = (
     event: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
   ) => {
@@ -33,18 +45,69 @@ function RecruiterForm() {
     }));
   };
 
-  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(formData);
+  const validateForm = () => {
+    const errors: RecruiterFormErrors = {};
+
+    if (!formData.name.trim()) {
+      errors.name = 'Le nom du recruteur est obligatoire';
+    }
+
+    if (!formData.company.trim()) {
+      errors.company = "Le nom de l'entreprise est obligatoire";
+    }
+
+    if (!formData.email.trim()) {
+      errors.email = "L'adresse email est obligatoire";
+    } else {
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!emailPattern.test(formData.email)) {
+        errors.email = 'Renseignez une adresse email valide';
+      }
+    }
+
+    if (!formData.contract.trim()) {
+      errors.contract = 'Séléctionnez le type de contract';
+    }
+
+    if (!formData.workMode.trim()) {
+      errors.workMode = 'Séléctionnez un mode de travail';
+    }
+
+    if (!formData.message.trim()) {
+      errors.message = 'Renseigner un message';
+    } else if (formData.message.trim().length < 20) {
+      errors.message = 'Votre message doit au moins avoir 20 caractères';
+    }
+
+    return errors;
   };
 
+  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const errors = validateForm();
+
+    console.log(JSON.stringify(formErrors, null, 2));
+
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length > 0) {
+      return; // arrête immédiatement handleSubmit
+    }
+
+    console.log('Formulaire soumis avec succès', formData);
+  };
+
+  // Affichage
   return (
     <section>
       <h2>Tester mon profil avec une opportunité</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} noValidate>
         <div>
           <label htmlFor="name">Nom du recruteur</label>
           <input id="name" name="name" type="text" value={formData.name} onChange={handleChange} />
+          {formErrors.name && <p>{formErrors.name}</p>}
         </div>
 
         <div>
@@ -56,6 +119,7 @@ function RecruiterForm() {
             value={formData.company}
             onChange={handleChange}
           />
+          {formErrors.company && <p>{formErrors.company}</p>}
         </div>
 
         <div>
@@ -67,6 +131,7 @@ function RecruiterForm() {
             value={formData.email}
             onChange={handleChange}
           />
+          {formErrors.email && <p>{formErrors.email}</p>}
         </div>
 
         <div>
@@ -77,6 +142,7 @@ function RecruiterForm() {
             <option value="CDD">CDD</option>
             <option value="FREELANCE">Freelance</option>
           </select>
+          {formErrors.contract && <p>{formErrors.contract}</p>}
         </div>
 
         <div>
@@ -87,11 +153,13 @@ function RecruiterForm() {
             <option value="HYBRIDE">Hybride</option>
             <option value="PRESENTIEL">Présentiel</option>
           </select>
+          {formErrors.workMode && <p>{formErrors.workMode}</p>}
         </div>
 
         <div>
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
+          {formErrors.message && <p>{formErrors.message}</p>}
         </div>
 
         <button type="submit">Vérifier l’opportunité</button>
